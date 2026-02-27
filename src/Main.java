e7397ed7cba592c2fb7f1db90c4d223be3b475c0
patrickfitzgerald <fitzgepc@mail.uc.edu>
2026-02-27 14:28:33 -0500

    Task 6 complete

--- src/Main.java
+++ src/Main.java
@@ -81,3 +81,18 @@ class Task5 {
     }
 }
 
+class Task6 {
+    public static void main(String[] args)
+    {
+        for (int row = 1; row <= 5; row++)
+        {
+            System.out.println("");
+            for (int col = 5; col >= row; col--)
+            {
+                System.out.print("*\t");
+            }
+        }
+        System.out.println("");
+
+    }
+}
